030aca0a7793ba654e1f490a6d4fe9a5045eae38
tarvainen <atte.tarvainen@protacon.com>
2018-10-08 16:18:45 +0300

    VF-11715 Implement translations part 12

--- src/Component/UndoButton.js
+++ src/Component/UndoButton.js
@@ -3,15 +3,18 @@ import Tooltip from '@material-ui/core/Tooltip/Tooltip';
 import IconButton from '@material-ui/core/IconButton/IconButton';
 import Undo from '@material-ui/icons/Undo';
 import PropTypes from 'prop-types';
+import { withNamespaces } from 'react-i18next';
 
 class UndoButton extends Component {
   render() {
+    const { t } = this.props;
+
     return (
-      <Tooltip title="Undo">
+      <Tooltip title={t('doUndo')}>
         <div>
           <IconButton
             color="inherit"
-            aria-label="Undo"
+            aria-label={t('doUndo')}
             onClick={this.props.onUndo}
             disabled={!this.props.canUndo}
           >
@@ -28,4 +31,4 @@ UndoButton.propTypes = {
   onUndo: PropTypes.func.isRequired
 };
 
-export default UndoButton;
+export default withNamespaces()(UndoButton);
