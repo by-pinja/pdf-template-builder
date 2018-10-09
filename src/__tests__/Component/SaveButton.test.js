eabc7e27b2edbcb67909e4e9685c30e04bbbd7cc
tarvainen <atte.tarvainen@protacon.com>
2018-10-09 15:24:13 +0300

    VF-11969 Add more tests

---
+++ src/__tests__/Component/SaveButton.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SaveButton from '../../Component/SaveButton';
+import { shallow } from 'enzyme';
+import IconButton from '@material-ui/core/IconButton/IconButton';
+
+describe('<SaveButton />', () => {
+  describe('render()', () => {
+    test('will render a button if onSaveTemplate() callback is given', () => {
+      const mockOnSave = jest.fn();
+
+      const wrapper = shallow(
+        <SaveButton onSaveTemplate={mockOnSave} />
+      );
+
+      expect(wrapper.html()).not.toBe(null);
+    });
+
+    test('will render nothing is callback is not given', () => {
+      const wrapper = shallow(
+        <SaveButton />
+      );
+
+      expect(wrapper.html()).toBe(null);
+    });
+  });
+
+  describe('onClick()', () => {
+    test('triggers onSave callback', () => {
+      const mockOnSave = jest.fn();
+
+      const wrapper = shallow(
+        <SaveButton onSaveTemplate={mockOnSave} />
+      );
+
+      const component = wrapper.dive();
+      component.find(IconButton).simulate('click');
+
+      expect(mockOnSave.mock.calls.length).toEqual(1);
+    });
+  });
+});
